8f58e9cd31cefe1fa6cb1e6b698566465ad1c76b
hwaj2 <shinh3222@gmail.com>
2023-03-01 20:57:55 +0900

    chore : 메모 추가

--- sql/create_data.sql
+++ sql/create_data.sql
@@ -6,8 +6,17 @@ CREATE TABLE USERS(
 );
 
 
+
 SELECT * FROM USERS;
 SELECT * FROM POST;
 
-DELETE FROM POST WHERE ID=6;
-COMMIT;
+
+// 카카오 api 요청 정보
+// REST API : 494d815f5517043247cb1f2773b09196
+// 도메인 : http://localhost:8080
+// redirect uri : http://localhost:8080/oauth/kakao
+
+// google api 정보
+// 클라이언트 아이디 : 104242137738-fqqajh0r2jkrc76ein0fpe30dhbba9a5.apps.googleusercontent.com
+// 클라이언트 비밀번호 : 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
+// redirect uri : http://localhost:8080/oauth2/code/google
