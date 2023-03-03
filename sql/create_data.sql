39ce02a0bb4d53fea5cc2369c107dd210cd87bc6
hwaj2 <shinh3222@gmail.com>
2023-03-04 01:37:59 +0900

    chore : api 기록 정보 수정

--- sql/create_data.sql
+++ sql/create_data.sql
@@ -12,11 +12,11 @@ SELECT * FROM POST;
 
 
 // 카카오 api 요청 정보
-// REST API : 494d815f5517043247cb1f2773b09196
+// 앱키(client ID) : 1c2685d26cbbd907d5fb9764e52454af
 // 도메인 : http://localhost:8080
 // redirect uri : http://localhost:8080/oauth/kakao
 
 // google api 정보
-// 클라이언트 아이디 : 104242137738-fqqajh0r2jkrc76ein0fpe30dhbba9a5.apps.googleusercontent.com
+// 앱키(client ID) : 104242137738-fqqajh0r2jkrc76ein0fpe30dhbba9a5.apps.googleusercontent.com
 // 클라이언트 비밀번호 : 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
 // redirect uri : http://localhost:8080/oauth2/code/google
