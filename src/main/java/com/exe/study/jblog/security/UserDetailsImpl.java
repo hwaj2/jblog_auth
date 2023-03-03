a7a74767baee345ccbc0b7bc2683d0276465ad8f
hwaj2 <shinh3222@gmail.com>
2023-03-04 02:09:38 +0900

    feat : UserDetailsImpl OAuth2User추가 및 수정

--- src/main/java/com/exe/study/jblog/security/UserDetailsImpl.java
+++ src/main/java/com/exe/study/jblog/security/UserDetailsImpl.java
@@ -5,18 +5,41 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 @Getter @Setter
-public class UserDetailsImpl implements UserDetails {
+public class UserDetailsImpl implements UserDetails, OAuth2User {
 
     private static final long serialVersionUID= 1L;
     private User user;
+    private Map<String, Object> attributes; //구글에서 조회한 사용자 정보를 담을 컬렉션
 
+
+    // 기본생성자
     public UserDetailsImpl(User user) {
         this.user = user;
+    }
+
+    // OAuth 로그인시 사용할 생성자
+    public UserDetailsImpl(User user, Map<String, Object> attributes){
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+
+    // OAuth2 - 구글에서 조회한 사용자 정보가 저장된 컬렉션 반환 = 인증서버가 인증클라이언트에게 전달할 리소스 오너의 정보가 저장
+    @Override
+    public Map<String, Object> getAttributes(){
+        return attributes;
+    }
+    // OAuth2 -이름은 사용하지 않는 정보이므로 null반환
+    @Override
+    public String getName() {
+        return null;
     }
 
 
